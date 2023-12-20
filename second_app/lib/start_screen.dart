5daecba58baf19c88ae431b32ff13781d2a1fa9e
visarut <visarut@netbay.co.th>
2023-12-20 17:21:40 +0700

    Update start screen

--- second_app/lib/start_screen.dart
+++ second_app/lib/start_screen.dart
@@ -24,10 +24,11 @@ class StartScreen extends StatelessWidget {
         const SizedBox(
           height: 30,
         ),
-        OutlinedButton(
+        OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            child: const Text('Start Quiz'))
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'))
       ],
     ));
   }
